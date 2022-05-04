bf64f0a76f96ebeb67dc1611b65c3621a2511e53
zffgithub <zffforgithub@gmail.com>
2022-05-04 11:10:18 +0800

    fix py client request

--- example/python/client.py
+++ example/python/client.py
@@ -8,7 +8,7 @@ import cyber_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = cyber_pb2_grpc.CyberManagerStub(channel)
-        response = stub.RemoteControl(cyber_pb2.HelloRequest(name='horika'))
+        response = stub.RemoteControl(cyber_pb2.Request(cyber_id=2))
     print("调用成功: {}!".format(response))
 
 if __name__ == '__main__':
