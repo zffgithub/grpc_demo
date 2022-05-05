4ee5c7581a532b9c062354a6cc119c9cedd76197
zffgithub <zffforgithub@gmail.com>
2022-05-05 10:27:04 +0800

    mod max con age

--- example/golang/server/server.go
+++ example/golang/server/server.go
@@ -164,11 +164,11 @@ func main() {
 	}
 
 	kasp := keepalive.ServerParameters{
-		MaxConnectionIdle:     180 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-		MaxConnectionAge:      90 * time.Second,  // If any connection is alive for more than 30 seconds, send a GOAWAY
-		MaxConnectionAgeGrace: 5 * time.Second,   // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-		Time:                  5 * time.Second,   // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-		Timeout:               3 * time.Second,   // Wait 1 second for the ping ack before assuming the connection is dead
+		MaxConnectionIdle:     180 * time.Second,  // If a client is idle for 15 seconds, send a GOAWAY
+		MaxConnectionAge:      3600 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+		MaxConnectionAgeGrace: 5 * time.Second,    // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+		Time:                  5 * time.Second,    // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+		Timeout:               3 * time.Second,    // Wait 1 second for the ping ack before assuming the connection is dead
 	}
 
 	opts := []grpc.ServerOption{
